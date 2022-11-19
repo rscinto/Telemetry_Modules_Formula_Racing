6a2cda0742da40d20f5b1c4675d97ab2d91383d5
DESKTOP-8CS8DSE\rocco <rocco.scinto@gmail.com>
2022-11-19 11:38:21 -0800

    adding images for read me

--- Working Modules/Teensy_UART_TX/Teensy_UART_TX.ino
+++ Working Modules/Teensy_UART_TX/Teensy_UART_TX.ino
@@ -1,35 +1,13 @@
 #define HWSERIAL Serial1
 
-
-class class_message
+void setup() 
 {
-  public:
-    static int message_number;
-    float data1;
-    float data2;
-    float data3;
-    float data4;
-    float data5;
-    float data6;
-    float data7;
-    float data8;
-    float data9;
-    float data10;
-    class_message(){ message_number++;}
-  
-};
-
-
-
-void setup() {
-  
   Serial.begin(74880);
   HWSERIAL.begin(9600, SERIAL_8N1);
 }
  
 void loop() 
 {
-
   for(float modulator= 50; modulator < 150; modulator++)
   {
     float* sender = &modulator;
@@ -37,9 +15,5 @@ void loop()
     HWSERIAL.write(byteData, 4);
     Serial.println(modulator);
     delay(50);
-  }
-    //Serial.println("Sending Test");
-    //HWSERIAL.print("TEST");
-
-  
+  } 
 }
